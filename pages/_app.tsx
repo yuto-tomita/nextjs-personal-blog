384071216017759ff9bfd7899ab42ad1faf58a0e
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-10-03 18:35:57 +0900

    lintfix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Head } from '@/components/common'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp ({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head />
