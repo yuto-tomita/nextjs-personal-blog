47f3beba293412e67f067dc28224c5096051f308
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-10-10 23:22:25 +0900

    Headerコンポーネントからページにアクセスできるように加筆

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -1,18 +1,26 @@
 import React, { FC, useState } from 'react'
+import { useRouter } from 'next/router'
 import { Menu } from 'antd'
 
 const Header: FC = () => {
-	const [menuState, setSelectMenu] = useState('0')
+	// TODO: Google上でブログ記事がクリックされたときに選択されているmenuが'1'になるようにする
+	const [menuState, setSelectMenu] = useState('')
+	const router = useRouter()
 
 	const handleClick = (event: any) => {
-		setSelectMenu(event.key)
+		const menuName = event.key.toLowerCase()
+		setSelectMenu(menuName)
+	}
+
+	const navigateToLink = (menuName: string) => {
+		router.push(menuName)
 	}
 
 	return (
 		<header>
 			<Menu mode="horizontal" selectedKeys={[menuState]} onClick={handleClick}>
 				{['Home', 'Blog', 'Contact'].map((menuName, index) => (
-					<Menu.Item key={index}>{menuName}</Menu.Item>
+					<Menu.Item key={menuName.toLowerCase()} onClick={() => navigateToLink(menuName)}>{menuName}</Menu.Item>
 				))}
 			</Menu>
 		</header>
