b461b0c1039f0ada0c657eebf85a46b0c15e6ab0
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-10-10 19:59:17 +0900

    Headerコンポーネントの作成

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -1,10 +1,20 @@
-import { FC } from 'react'
-
+import React, { FC, useState } from 'react'
+import { Menu } from 'antd'
 
 const Header: FC = () => {
+	const [menuState, setSelectMenu] = useState('0')
+
+	const handleClick = (event: any) => {
+		setSelectMenu(event.key)
+	}
+
 	return (
 		<header>
-			hogehoge
+			<Menu mode="horizontal" selectedKeys={[menuState]} onClick={handleClick}>
+				{['Home', 'Blog', 'Contact'].map((menuName, index) => (
+					<Menu.Item key={index}>{menuName}</Menu.Item>
+				))}
+			</Menu>
 		</header>
 	)
 }
