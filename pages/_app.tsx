97417c41d861058a33ccdee2ec6d0854412384ce
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-10-10 18:45:21 +0900

    ant-designを導入するためのファイルをインポート

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Head } from '@/components/common'
-import { Header } from '@/components/common'
+import { Head, Header } from '@/components/common'
+import 'antd/dist/antd.css'
 
 function MyApp ({ Component, pageProps }: AppProps) {
   return (
