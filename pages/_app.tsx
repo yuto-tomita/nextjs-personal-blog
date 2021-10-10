a38f89cd2502f4a0c4e1c29e9db00412aeeea1f0
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-10-10 22:45:16 +0900

    SSRする際にant-designからエラーが起きていたため修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,27 @@
+import { FC, useEffect, useState } from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Head, Header } from '@/components/common'
 import 'antd/dist/antd.css'
 
 function MyApp ({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head />
-      <Header></Header>
-      <Component {...pageProps} />
-    </>
-  )
+  // ant-designのMenuコンポーネントを使用するにはSSRだとエラーが起きるため画面に要素が描画されてからコンポーネントを描画するようにする
+  const [showComponent, setShowComponent] = useState(false)
+
+  useEffect(() => {
+    setShowComponent(true)
+  }, [])
+
+  if (showComponent) {
+    return (
+      <>
+        <Head />
+        <Header />
+        <Component {...pageProps} />
+      </>
+    )
+  } else {
+    return null
+  }
 }
 export default MyApp
