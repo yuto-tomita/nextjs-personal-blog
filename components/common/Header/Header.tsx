7535fb65049473be600a0b1de653f2ea04a5d06b
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-10-10 16:28:36 +0900

    Headerコンポーネントを作成

---
+++ components/common/Header/Header.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+
+
+const Header: FC = () => {
+	return (
+		<header>
+			hogehoge
+		</header>
+	)
+}
+
+export default Header
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Head } from '@/components/common'
+import { Header } from '@/components/common'
 
 function MyApp ({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head />
+      <Header></Header>
       <Component {...pageProps} />
     </>
   )
