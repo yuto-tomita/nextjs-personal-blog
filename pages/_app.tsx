7f41b739d8dff03ce0040373a556ddcae5602b3b
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-10-13 20:57:04 +0900

    パスエイリアスを変更したため修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { FC, useEffect, useState } from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Head, Header } from '@/components/common'
+import { Head, Header } from '@components/common'
 import 'antd/dist/antd.css'
 
 function MyApp ({ Component, pageProps }: AppProps) {
