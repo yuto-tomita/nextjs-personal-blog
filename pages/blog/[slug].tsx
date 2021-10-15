885a03cf295fb43dd9de521605005a2d176877ee
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-10-15 23:01:45 +0900

    個別mdファイルが表示されるように加筆

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,13 +1,42 @@
-import { useRouter } from 'next/router';
+import { getMdFileFromDir, readFileFromFileName, parseMdFile } from '@lib/MdFileOperation'
+import type { InferGetStaticPropsType } from 'next'
 
-const Post = () => {
-  const router = useRouter();
-  const slug = router.query.slug;
-	console.log(router)
+export async function getStaticPaths () {
+	const mdFileNames = getMdFileFromDir()
+	const mdFile = mdFileNames.map(fileName => readFileFromFileName(fileName))
+	const paths = mdFile.map(markdown => {
+		const parseMdContent = parseMdFile(markdown)
+
+		return `/blog/${parseMdContent.data.slug}`
+	})
+
+	return {
+		paths,
+		fallback: false
+	}
+}
+
+export async function getStaticProps (context: any) {
+	const { slug } = context.params
+	const mdFileContent = readFileFromFileName(`${slug}.md`)
+	const parseMdContent = parseMdFile(mdFileContent)
+
+	return {
+		props: {
+			title: parseMdContent.data.title,
+			content: parseMdContent.content
+		}
+	}
+}
+
+const Post = ({
+	title,
+	content
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
 		<div>
-			<p>Post: {slug}</p>
-			[router]
+			<p>Title: {title}</p>
+			<p>Content: {content}</p>
 		</div>
 	)
 };
