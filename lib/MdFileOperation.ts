256bd1ae8c75f45923419718f7e0c52ef255c28c
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-10-24 14:33:34 +0900

    より拡張性が高くファイル読み込みができるように加筆

--- lib/MdFileOperation.ts
+++ lib/MdFileOperation.ts
@@ -1,14 +1,19 @@
 import fs from 'fs'
 import matter from 'gray-matter'
 
-export function getMdFileFromDir () {
-	return fs.readdirSync('./articles')
+type DirName = 'resume' | 'article'
+
+/** 引数のディレクトリ名からディレクトリ配下のファイル名を取得する */
+export function getMdFileFromDir (dirName: string) {
+	return fs.readdirSync(`./${dirName}`)
 }
 
-export function readFileFromFileName (fileName: string) {
-	return fs.readFileSync(`./articles/${fileName}`, 'utf8')
+/** 引数のファイル名から、引数のディレクトリ名配下のファイルを読み込む */
+export function readFileFromFileName (fileName: string, dirName: DirName) {
+	return fs.readFileSync(`./${dirName}/${fileName}`, 'utf8')
 }
 
+/** 読み込んだファイル名を扱いやすいデータ形式に変換する */
 export function parseMdFile (mdFile: string) {
 	return matter(mdFile)
 }
