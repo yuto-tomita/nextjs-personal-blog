f039acc704a68bb059777f63b6a8a7621f16d4e6
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-10-29 18:34:38 +0900

    ページ一番下部にFooterが表示されるように修正

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,5 +1,14 @@
 import type { NextPage } from 'next'
+import { Container } from '@components/ui'
 
-const Contact: NextPage = () => <div>test</div>
+const Contact: NextPage = () => {
+	return (
+		<Container>
+			<div>
+				test
+			</div>
+		</Container>
+	)
+}
 
 export default Contact
