e06cc7c14e6090fb861ba3c4e8db9c48c0a676eb
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-11-06 00:33:03 +0900

    GoogleSearchConsole対応

--- components/common/Head/Head.tsx
+++ components/common/Head/Head.tsx
@@ -1,14 +1,14 @@
 import { FC } from 'react'
-// import NextHead from 'next/head'
+import NextHead from 'next/head'
 
 const Head: FC = () => {
 	return (
 		<>
-			<head>
+			<NextHead>
 				<title>とみーの個人ブログ</title>
 				<meta name="google-site-verification" content="ddrRraQeMATOMed5-3KD2HIgTqnuT0FsXze03hiaNYI" />
 				<meta name="viewport" content="width=device-width, initial-scale=1"/>
-			</head>
+			</NextHead>
 		</>
 	)
 }
