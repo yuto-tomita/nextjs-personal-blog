49a9579b03dcf15f3bb4189cdf5292394b942112
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-11-06 00:05:09 +0900

    SearchConsoleのためのmetaタグ追加

--- components/common/Head/Head.tsx
+++ components/common/Head/Head.tsx
@@ -6,6 +6,7 @@ const Head: FC = () => {
 		<>
 			<NextHead>
 				<title>とみーの個人ブログ</title>
+				<meta name="google-site-verification" content="ddrRraQeMATOMed5-3KD2HIgTqnuT0FsXze03hiaNYI" />
 				<meta name="viewport" content="width=device-width, initial-scale=1"/>
 			</NextHead>
 		</>
