47e29addff1459603920ded7e0a2af50b353f5f4
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-11-09 22:10:45 +0900

    フォームを設置

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,11 +1,46 @@
 import type { NextPage } from 'next'
 import { Container } from '@components/ui'
+import { Input } from 'antd'
+import { useMail } from '@lib/hooks/useMail'
+import style from '../styles/Contact.module.css'
 
 const Contact: NextPage = () => {
+	const { setMail, setName, setSubject, setBody } = useMail()
+	const { TextArea } = Input
+
 	return (
 		<Container>
-			<div>
-				test
+			<div className={style.formContainer}>
+				<label>件名</label>
+				<Input
+				  placeholder="件名を入力してください"
+					allowClear
+					onChange={(e) => setSubject(e.target.value)}
+				/>
+			</div>
+			<div className={style.formContainer}>
+				<label>メールアドレス</label>
+				<Input
+					placeholder="メールアドレスを入力してください"
+					allowClear
+					onChange={(e) => setMail(e.target.value)}
+				/>
+			</div>
+			<div className={style.formContainer}>
+				<label>名前</label>
+				<Input
+					placeholder="名前を入力してください"
+					allowClear
+					onChange={(e) => setName(e.target.value)}
+				/>
+			</div>
+			<div className={style.formContainer}>
+				<label>お問い合わせ内容</label>
+				<TextArea
+					placeholder="お問い合わせ内容を入力してください"
+					allowClear
+					onChange={(e) => setBody(e.target.value)}
+				/>
 			</div>
 		</Container>
 	)
