d22cffeb31052c22c2e35fef20183c3605accd0f
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-11-12 21:41:14 +0900

    メール送信ができるように加筆

---
+++ pages/api/mail.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { createTransport } from 'nodemailer';
+
+export default async function handler (
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	const transporter = createTransport({
+		service: 'gmail',
+		port: 465,
+		secure: true,
+		auth: {
+			user: process.env.MAIL_USER,
+			pass: process.env.MAIL_PASS,
+		},
+	});
+	await transporter.sendMail({
+	  from: process.env.MAIL_FROM,
+	  to: process.env.MAIL_FROM,
+	  subject: 'お問い合わせ',
+	  text: req.body,
+	});
+	res.status(200).json({
+		success: true,
+	});
+}
