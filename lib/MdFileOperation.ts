6dc0e9bfdc9e745dcd34d08784a1020bcdaef22f
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-11-13 13:23:07 +0900

    同的にサイトマップを生成するメソッドを記述

--- lib/MdFileOperation.ts
+++ lib/MdFileOperation.ts
@@ -20,3 +20,25 @@ export function readFileFromFileName (fileName: string, dirName: DirName) {
 export function parseMdFile (mdFile: string) {
 	return matter(mdFile)
 }
+
+function getPathAndCreateDate (dirName: DirName) {
+	return getMdFileFromDir(dirName).map(val => {
+		const mdFile = readFileFromFileName(val, dirName)
+		const getParseMdFile = parseMdFile(mdFile)
+
+		return {
+			path: `${join(process.cwd(), dirName)}/${val}`,
+			lastmod: getParseMdFile.data.created_at
+		}
+	})
+}
+
+/** mdファイルが格納されているディレクトリからpath名を取得する */
+export function getAllPostsPath () {
+	return {
+		posts: [
+			...getPathAndCreateDate('resume'),
+			...getPathAndCreateDate('teck-blog')
+		]
+	}
+}
