e82f3eba1db0dc81ec368695447eec4bc950d918
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-11-14 00:37:23 +0900

    syntaxhighliteが使用できてることを確認

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-children-prop */
 import { getMdFileFromDir, readFileFromFileName, parseMdFile } from '@lib/MdFileOperation'
 import type { InferGetStaticPropsType } from 'next'
 import { NextSeo, BlogJsonLd } from 'next-seo'
@@ -9,6 +10,9 @@ import { getSpanValue } from '@lib/GetArticleSpan'
 import { useWindowDimensions } from '@lib/hooks/useDetectScreenSize'
 import { StarOutlined, StarFilled } from '@ant-design/icons'
 import { useState } from 'react'
+import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { base16AteliersulphurpoolLight } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 export async function getStaticPaths () {
 	const mdFileNames = getMdFileFromDir('teck-blog')
@@ -126,7 +130,27 @@ const Post = ({
 							{title}
 						</h1>
 						<div className={style.content}>
-							{content}
+							<ReactMarkdown 
+							  children={content}
+								components={{
+									code ({node, inline, className, children, ...props}) {
+										const match = /language-(\w+)/.exec(className || '')
+										return !inline && match ? (
+											<SyntaxHighlighter
+												children={String(children).replace(/\n$/, '')}
+												style={base16AteliersulphurpoolLight}
+												language={match[1]}
+												// PreTag="div"
+												// {...props}
+											/>
+										) : (
+											<code className={className} {...props}>
+												{children}
+											</code>
+										)
+									}
+								}}
+							/>
 						</div>
 					</Col>
 
