7915e1f023ea76f58e299b58ae4fc85b8bfc05d3
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-11-16 00:07:39 +0900

    x軸とy軸のスクロール量を取得するhooksを作成

---
+++ lib/hooks/useScrollAmount.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+
+const getScrollAmount = () => {
+	const { pageXOffset: xaxisAmount, pageYOffset: yaxisAmount } = window
+
+	return {
+		xaxisAmount,
+		yaxisAmount
+	}
+}
+
+export const useScrollAmount = () => {
+	const [scrollAmount, setScrollAmount] = useState(getScrollAmount())
+
+	useEffect(() => {
+		const handleScroll = () => {
+			setScrollAmount(getScrollAmount())
+		}
+
+		window.addEventListener('scroll', handleScroll)
+		return () => window.removeEventListener('resize', handleScroll)
+	}, [])
+
+	return {
+		xaxisAmount: scrollAmount.xaxisAmount,
+		yaxisAmount: scrollAmount.yaxisAmount
+	}
+}
