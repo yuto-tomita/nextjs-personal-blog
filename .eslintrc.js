22c69c7eb904e9536ac69f5f97db380eea93a57e
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-11-24 21:18:01 +0900

    eslint設定周り加筆

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+    React: "writable",
+  },
+  ignorePatterns: ["build"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 2021,
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["@typescript-eslint", "react"],
+  rules: {
+    //　ここに個別対応したいものを書く
+  },
+  settings: { react: { version: "detect" } },
+};
