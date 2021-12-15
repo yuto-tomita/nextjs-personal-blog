93b21e7f9c84e6cbf6eafad2ccc1ae7e55539dbf
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-12-15 21:21:36 +0900

    preview用のコンポーネントの作成

---
+++ components/ui/Preview/MarkdownPreview.tsx
@@ -0,0 +1,50 @@
+import { FC } from 'react'
+import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { base16AteliersulphurpoolLight } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+
+interface Props {
+	markdownContent: string
+}
+
+const MarkdownPreview: FC<Props> = ({
+	markdownContent
+}) => {
+	return (
+  <>
+    <ReactMarkdown 
+      components={{
+				code ({
+          inline,
+          className,
+          children,
+          ...props
+        }) {
+					const match = /language-(\w+)/.exec(className || '')
+					return !inline && match
+          ? (
+            <SyntaxHighlighter
+              style={base16AteliersulphurpoolLight}
+              language={match[1]}
+            >
+              {String(children).replace(/\n$/, '')}
+            </SyntaxHighlighter>
+					)
+          : (
+            <code
+              className={className}
+              {...props}
+            >
+              {children}
+            </code>
+					)
+				}
+      }}
+    >
+      {markdownContent}
+    </ReactMarkdown>
+  </>
+	)
+}
+
+export default MarkdownPreview
