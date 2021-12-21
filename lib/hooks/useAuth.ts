808866c1a31e383fd9334cd400fdcaa1bc862bcf
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-12-21 22:25:43 +0900

    Session情報を取得できるように加筆

--- lib/hooks/useAuth.ts
+++ lib/hooks/useAuth.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import type { Session } from '@supabase/supabase-js'
 
 const supabaseUrl = () => process.env.NEXT_PUBLIC_SUPABASE_URL || ''
 const supabaseAnonKey = () => process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
@@ -7,16 +8,20 @@ const supabaseAnonKey = () => process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
 const supabase = createClient(supabaseUrl(), supabaseAnonKey())
 
 export const useAuth = () => {
+	const [session, setSession] = useState<Session | null>(null)
+
+	useEffect(() => {
+		supabase.auth.onAuthStateChange((_event, session) => {
+			setSession(session)
+		})
+	}, [])
+
 	const signInGithub = async () => {
 		await supabase.auth.signIn({ provider: 'github'})
 	}
 
-	const guard = () => {
-		return !!localStorage.getItem('accessToken')
-	}
-
 	return {
 		signInGithub,
-		guard
+		session
 	}
 }
