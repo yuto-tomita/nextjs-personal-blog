5f52547f9c1af103e97dbcd8ee65f9360f29959b
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-12-22 22:52:48 +0900

    sessionを返すメソッドを記述

--- lib/hooks/useAuth.ts
+++ lib/hooks/useAuth.ts
@@ -6,13 +6,7 @@ const supabaseAnonKey = () => process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
 const supabase = createClient(supabaseUrl(), supabaseAnonKey())
 
 export const useAuth = () => {
-	const hasSessionToSignIn = () => {
-		const session = supabase.auth.session()
-		
-		if (!session) {
-			signInGithub()
-		}
-	}
+	const session = () => supabase.auth.session()
 
 	const signInGithub = async () => {
 		await supabase.auth.signIn({ provider: 'github'})
@@ -20,6 +14,6 @@ export const useAuth = () => {
 
 	return {
 		signInGithub,
-		hasSessionToSignIn
+		session
 	}
 }
