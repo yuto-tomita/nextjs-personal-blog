905e676b925717ef4ba3fe8ee4c59df730d3bdc0
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-12-22 22:41:25 +0900

    supabaseにsessionが存在するか確認し、なければgithubログインするように加筆

--- lib/hooks/useAuth.ts
+++ lib/hooks/useAuth.ts
@@ -1,6 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-import { useState, useEffect } from 'react'
-import type { Session } from '@supabase/supabase-js'
 
 const supabaseUrl = () => process.env.NEXT_PUBLIC_SUPABASE_URL || ''
 const supabaseAnonKey = () => process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
@@ -8,13 +6,13 @@ const supabaseAnonKey = () => process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
 const supabase = createClient(supabaseUrl(), supabaseAnonKey())
 
 export const useAuth = () => {
-	const [session, setSession] = useState<Session | null>(null)
-
-	useEffect(() => {
-		supabase.auth.onAuthStateChange((_event, session) => {
-			setSession(session)
-		})
-	}, [])
+	const hasSessionToSignIn = () => {
+		const session = supabase.auth.session()
+		
+		if (!session) {
+			signInGithub()
+		}
+	}
 
 	const signInGithub = async () => {
 		await supabase.auth.signIn({ provider: 'github'})
@@ -22,6 +20,6 @@ export const useAuth = () => {
 
 	return {
 		signInGithub,
-		session
+		hasSessionToSignIn
 	}
 }
