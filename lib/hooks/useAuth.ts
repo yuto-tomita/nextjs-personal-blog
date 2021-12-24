c888bd6523ae6517f82cb4f84e1d58e57dbc4857
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-12-25 01:33:58 +0900

    sessionの値をlocalStorageに保持するように加筆

--- lib/hooks/useAuth.ts
+++ lib/hooks/useAuth.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import type { Session } from '@supabase/supabase-js'
+import { useLocalStorage } from '@lib/hooks/useLocalStorage'
 
 const supabaseUrl = () => process.env.NEXT_PUBLIC_SUPABASE_URL || ''
 const supabaseAnonKey = () => process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
@@ -8,7 +9,7 @@ const supabaseAnonKey = () => process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
 const supabase = createClient(supabaseUrl(), supabaseAnonKey())
 
 export const useAuth = () => {
-	const [session, setSession] = useState<null | Session>(null)
+	const [session, setSession] = useLocalStorage<null | Session>('session', null)
 
 	useEffect(() => {
 		supabase.auth.onAuthStateChange((_event, session) => {
