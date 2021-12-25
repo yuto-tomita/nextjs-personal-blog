fde3b7ee153748c34b2ec3a0653afe6766fbbec7
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-12-25 17:02:38 +0900

    navigationGuardを実行するhookを追加

--- lib/hooks/useAuth.ts
+++ lib/hooks/useAuth.ts
@@ -2,6 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 import { useEffect } from 'react'
 import type { Session } from '@supabase/supabase-js'
 import { useLocalStorage } from '@lib/hooks/useLocalStorage'
+import Router from 'next/router'
 
 const supabaseUrl = () => process.env.NEXT_PUBLIC_SUPABASE_URL || ''
 const supabaseAnonKey = () => process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
@@ -25,9 +26,20 @@ export const useAuth = () => {
 		return supabase.auth.session()
 	}
 
+	const navigationGurad = () => {
+		if (session === null) {
+			signInGithub()
+		} else {
+			if (session.user?.email !== 'qualidea01@gmail.com') {
+				Router.push('/')
+			}
+		}
+	}
+
 	return {
 		signInGithub,
 		session,
-		isExistSession
+		isExistSession,
+		navigationGurad
 	}
 }
