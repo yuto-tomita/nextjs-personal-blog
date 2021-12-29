7002a1189a7884e109e12dc21dbcc7064fbd160f
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-12-29 23:16:24 +0900

    バリデーションチェックを記述

---
+++ lib/ArticleFormValidationRule.ts
@@ -0,0 +1,22 @@
+export interface Values {
+  title: string
+  slug: string
+  image: string
+}
+
+type ErrorsMessage = Partial<Values>
+
+export default function validate(values: Values) {
+  const errors: ErrorsMessage = {}
+
+  if (!values.title.length) {
+    errors.title = 'タイトルが未入力'
+  }
+  if (!values.slug.length) {
+    errors.slug = 'slugが未入力'
+  }
+  if (!values.image.length) {
+    errors.image = '画像ファイル名が未入力'
+  }
+  return errors
+}
