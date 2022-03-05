df7d0301e60c7d6543ac0cb00b62b023faba79f7
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-03-05 12:34:18 +0900

    記事一覧ページのUIを一新

--- components/ui/List/List.tsx
+++ components/ui/List/List.tsx
@@ -1,12 +1,39 @@
-import { FC, CSSProperties } from 'react';
+import { FC, CSSProperties } from 'react'
+import style from './List.module.css'
+import Link from 'next/link'
 
+interface ArticleInfo {
+  title: string
+  description: string
+  href: string
+  created_at: string
+}
 interface ListProps {
-	dataSorce: []
-	style?: CSSProperties
+  articleInfo: ArticleInfo[]
 }
 
-const List: FC<ListProps> = () => {
-	return (<></>)
+const List: FC<ListProps> = ({ articleInfo = [] }) => {
+  return (
+    <div className={style.listWrapper}>
+      <ul>
+        {articleInfo.map((val, key) => {
+          return (
+            <Link href={`/blog/${val.href}`} key={key}>
+              <li className={style.list}>
+                <div className={style.listPostDate}>
+                  {val.created_at}
+                </div>
+                <div className={style.listTitle}>{val.title}</div>
+                <div className={style.listDescription}>
+                  {val.description}
+                </div>
+              </li>
+            </Link>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
 
 export default List
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -4,14 +4,8 @@ import {
   readFileFromFileName,
   parseMdFile
 } from '@lib/MdFileOperation'
-import Link from 'next/link'
-import { Row, Col, Card, Typography } from 'antd'
-import Image from 'next/image'
-import { Container } from '@components/ui'
-import { getSpanValue } from '@lib/GetArticleSpan'
-import { useWindowDimensions } from '@lib/hooks/useDetectScreenSize'
+import { Container, List } from '@components/ui'
 import dayjs from 'dayjs'
-import style from '../styles/Home.module.css'
 
 export async function getStaticProps() {
   const mdFileNames = getMdFileFromDir('teck-blog')
@@ -44,42 +38,17 @@ export async function getStaticProps() {
 const Blog = ({
   parseMarkdownContent
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const { Meta } = Card
-  const { width } = useWindowDimensions()
-
-  const { Title } = Typography
+  const listTitles = parseMarkdownContent.map((val) => {
+    return {
+      title: String(val.title),
+      description: String(val.description),
+      href: val.slug,
+      created_at: dayjs(val.created_at).format('YYYY-MM-DD')
+    }
+  })
   return (
     <Container>
-      <Row gutter={[48, 48]}>
-        {parseMarkdownContent.map((mdContents, index) => (
-          <Col key={index} span={getSpanValue(width)}>
-            <Card hoverable>
-              <Link href={`/blog/${mdContents.slug}`} key={index} passHref>
-                <div>
-                  <Image
-                    src={`/${mdContents.image}`}
-                    alt="blog rogo"
-                    width={500}
-                    height={300}
-                  />
-                  <Meta
-                    title={
-                      <Title
-                        level={5}
-                        ellipsis={false}
-                        className={style.titleWrap}
-                      >
-                        {mdContents.title}
-                      </Title>
-                    }
-                    description={mdContents.description}
-                  />
-                </div>
-              </Link>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+      <List articleInfo={listTitles} />
     </Container>
   )
 }
