e400497bd1a65443211ff7c24443e50285d60490
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-03-06 15:39:49 +0900

    paginationUIを実装

--- components/ui/Pagination/Pagination.tsx
+++ components/ui/Pagination/Pagination.tsx
@@ -1,7 +1,43 @@
 import { FC } from 'react'
+import style from './Pagination.module.css'
+import cn from 'classnames'
 
-const Pagination: FC = () => {
-  return <div>hoge</div>
+interface PaginationProps {
+  currentPage: number
+  onClick: (arg: number) => void
+  data: unknown[]
+  className?: string
+}
+
+const Pagination: FC<PaginationProps> = ({
+  currentPage,
+  onClick,
+  data = [],
+  className = ''
+}) => {
+  const pageCount = Math.ceil(data.length / 10)
+
+  return (
+    <div className={style.paginationWrapper}>
+      {[...Array(pageCount)].map((_, index) => {
+        return (
+          <div
+            className={cn(
+              style.pagination,
+              currentPage === index + 1
+                ? style.currentPagination
+                : '',
+              className
+            )}
+            key={index}
+            onClick={() => onClick(index + 1)}
+          >
+            {index + 1}
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Pagination
