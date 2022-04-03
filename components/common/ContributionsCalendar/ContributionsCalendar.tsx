fa6b24e2a6f39c8f7616e8fa78646ca4bf2791bb
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-04-03 12:26:51 +0900

    軽いスタイリング

--- components/common/ContributionsCalendar/ContributionsCalendar.tsx
+++ components/common/ContributionsCalendar/ContributionsCalendar.tsx
@@ -1,7 +1,42 @@
 import { FC } from 'react'
+import style from './Calendar.module.css'
+import type { ContributionsCalendarQuery } from 'queries/__generated__/ContributionsCalendarQuery.graphql'
+interface ContributionsCalendarProps {
+  contributionsCalendarData: ContributionsCalendarQuery['response']
+}
 
-const ContributionsCalendar: FC = () => {
-  return <div>hogehoge</div>
+const ContributionsCalendar: FC<ContributionsCalendarProps> = ({
+  contributionsCalendarData
+}) => {
+  console.log(contributionsCalendarData)
+  const weekContributions = () => {
+    if (contributionsCalendarData.user) {
+      return contributionsCalendarData.user?.contributionsCollection.contributionCalendar.weeks
+    } else {
+      return []
+    }
+  }
+  
+  
+  return (
+    <div className={style.calendarContainer}>
+      {
+        weekContributions().map((val, key) => {
+          return (
+            <div className={style.week} key={key}>
+              {val.contributionDays.map((val2, index2) => {
+                return (
+                  <div key={index2} className={style.day}>
+                    {val2.contributionCount}
+                  </div>
+                )
+              })}
+            </div>
+          )
+        })
+      }
+    </div>
+  )
 }
 
 export default ContributionsCalendar
