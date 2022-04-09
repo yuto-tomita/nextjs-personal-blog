f2ed47359305da15a23a528de31d1c52bd994101
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-04-09 20:45:58 +0900

    Tooltipコンポーネントのファイル作成

---
+++ components/ui/Tooltip/Tooltip.tsx
@@ -0,0 +1,8 @@
+import { FC, ReactNode } from 'react'
+import style from './Tooltip.module.css'
+
+const Tooltip: FC = ({ children }) => {
+  return <div>{children}</div>
+}
+
+export default Tooltip
