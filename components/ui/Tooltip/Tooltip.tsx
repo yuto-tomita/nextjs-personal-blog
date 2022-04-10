62c88158382e1561dc22eb1db19cb4e31d341a60
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-04-10 15:16:13 +0900

    Tooltopコンポーネントの削除

--- components/ui/Tooltip/Tooltip.tsx
+++
@@ -1,8 +0,0 @@
-import { FC, ReactNode } from 'react'
-import style from './Tooltip.module.css'
-
-const Tooltip: FC = ({ children }) => {
-  return <div>{children}</div>
-}
-
-export default Tooltip
