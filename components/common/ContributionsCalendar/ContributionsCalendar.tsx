ef9c27d042353e9ea321683ca8fc9d711cc2b45a
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-04-10 15:55:56 +0900

    Halloween仕様に草の色を変更をできるような機能の追加

--- components/common/ContributionsCalendar/ContributionsCalendar.tsx
+++ components/common/ContributionsCalendar/ContributionsCalendar.tsx
@@ -1,7 +1,8 @@
-import { FC, useMemo } from 'react'
+import { FC, useMemo, useState } from 'react'
 import style from './Calendar.module.css'
 import type { ContributionsCalendarQuery } from 'queries/__generated__/ContributionsCalendarQuery.graphql'
 import cn from 'classnames'
+import { Switch } from 'antd'
 
 interface ContributionsCalendarProps {
   contributionsCalendarData: ContributionsCalendarQuery['response']
@@ -10,6 +11,7 @@ interface ContributionsCalendarProps {
 const ContributionsCalendar: FC<ContributionsCalendarProps> = ({
   contributionsCalendarData
 }) => {
+  const [isHalloweenStatus, setIsHalloweenStatus] = useState(false)
   const average = useMemo(() => {
     if (contributionsCalendarData.user) {
       const total =
@@ -39,46 +41,59 @@ const ContributionsCalendar: FC<ContributionsCalendarProps> = ({
     if (contributionCount === 0) {
       return '#e8eaea'
     } else if (contributionCount < average + 4 && colors) {
-      return colors[0]
+      return isHalloweenStatus ? '#FFEE4A' : colors[0]
     } else if (contributionCount < average + 8 && colors) {
-      return colors[1]
+      return isHalloweenStatus ? '#FFC501' : colors[1]
     } else if (contributionCount < average + 12 && colors) {
-      return colors[2]
+      return isHalloweenStatus ? '#FE9600' : colors[2]
     } else if (colors) {
-      return colors[3]
+      return isHalloweenStatus ? '#03001C' : colors[3]
     }
   }
+
+  const switchStatusHalloweenColor = () => {
+    setIsHalloweenStatus(!isHalloweenStatus)
+  }
   return (
-    <div className={style.calendarContainer}>
-      {weekContributions().map((val, key) => {
-        return (
-          <div className={style.week} key={key}>
-            {val.contributionDays.map((val2, index2) => {
-              return (
-                <div
-                  key={index2}
-                  className={cn(style.day, style.tooltipContainer)}
-                  style={{
-                    background: getContributeColorClass(
-                      val2.contributionCount
-                    )
-                  }}
-                >
-                  <div className={cn(style.tooltipContents)}>
-                    {val2.date}
-                    <br />
-                    {val2.contributionCount}回
+    <>
+      <div className={style.calendarContainer}>
+        {weekContributions().map((val, key) => {
+          return (
+            <div className={style.week} key={key}>
+              {val.contributionDays.map((val2, index2) => {
+                return (
+                  <div
+                    key={index2}
+                    className={cn(style.day, style.tooltipContainer)}
+                    style={{
+                      background: getContributeColorClass(
+                        val2.contributionCount
+                      )
+                    }}
+                  >
+                    <div className={cn(style.tooltipContents)}>
+                      {val2.date}
+                      <br />
+                      {val2.contributionCount}回
+                    </div>
                   </div>
-                </div>
-              )
-            })}
-          </div>
-        )
-      })}
-      <span className={style.mondayLabel}>月</span>
-      <span className={style.wednesdayLabel}>水</span>
-      <span className={style.fridayLabel}>金</span>
-    </div>
+                )
+              })}
+            </div>
+          )
+        })}
+        <span className={style.mondayLabel}>月</span>
+        <span className={style.wednesdayLabel}>水</span>
+        <span className={style.fridayLabel}>金</span>
+      </div>
+      <div>
+        草の色をハロウィン仕様にする
+        <Switch
+          size="small"
+          onChange={() => switchStatusHalloweenColor()}
+        />
+      </div>
+    </>
   )
 }
 
