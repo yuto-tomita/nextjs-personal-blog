07235fcc3510c67f999161fb87b5fa10e45760af
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-04-18 21:16:41 +0900

    configファイルの作成

---
+++ config/next-seo.config.ts
@@ -0,0 +1,35 @@
+export default {
+  'titleTemplate': '%s | とみーの個人ブログ',
+  'description': '冨田優斗の個人ブログ',
+  'additionalMetaTags': [
+    {
+      'property': 'dc:creator',
+      'content': '冨田優斗'
+    },
+    {
+      'name': 'application-name',
+      'content': 'とみーの個人ブログ'
+    }
+  ],
+  'openGraph': {
+    'title': 'とみーの個人ブログ',
+    'description': 'とみーの個人ブログ - https://nextjs-personal-blog-five.vercel.app/',
+    'type': 'website',
+    'locale': 'ja_JP',
+    'url': 'https://nextjs-personal-blog-five.vercel.app/',
+    'site_name': 'とみーの個人ブログ',
+    'images': [
+      {
+        'url': '/MyProfileImage.jpeg',
+        'width': 800,
+        'height': 600,
+        'alt': 'とみーの個人ブログ'
+      }
+    ]
+  },
+  'twitter': {
+    'handle': '@冨田優斗',
+    'site': '@冨田優斗',
+    'cardType': 'summary_large_image'
+  }
+}
