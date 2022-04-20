c44895447a8e801db40d3ac75c59521c7717e608
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-04-20 21:23:15 +0900

    ogp周り設定リベンジ

--- config/next-seo.config.ts
+++ config/next-seo.config.ts
@@ -1,29 +1,29 @@
 export default {
-  titleTemplate: '%s | とみーの個人ブログ',
-  description: '冨田優斗の個人ブログ',
-  additionalMetaTags: [
-    {
-      'property': 'dc:creator',
-      'content': '冨田優斗'
-    },
-    {
-      'name': 'application-name',
-      'content': 'とみーの個人ブログ'
-    }
-  ],
+  // titleTemplate: '%s | とみーの個人ブログ',
+  // description: '冨田優斗の個人ブログ',
+  // additionalMetaTags: [
+  //   {
+  //     property: 'dc:creator',
+  //     content: '冨田優斗'
+  //   },
+  //   {
+  //     name: 'application-name',
+  //     content: 'とみーの個人ブログ'
+  //   }
+  // ],
   twitter: {
-    'handle': '@qualidea04',
-    'site': '@qualidea04',
-    'cardType': 'summary_large_image'
+    handle: '@qualidea04',
+    site: '@qualidea04',
+    cardType: 'summary_large_image'
   },
   openGraph: {
-    'title': 'とみーの個人ブログ',
-    'description': 'とみーの個人ブログ - https://nextjs-personal-blog-five.vercel.app/',
-    'type': 'website',
-    'locale': 'ja_JP',
-    'url': 'https://nextjs-personal-blog-five.vercel.app/',
-    'site_name': 'とみーの個人ブログ',
-    'images': [
+    title: 'とみーの個人ブログ',
+    description: 'とみーの個人ブログ - https://nextjs-personal-blog-five.vercel.app/',
+    type: 'website',
+    locale: 'ja_JP',
+    url: 'https://nextjs-personal-blog-five.vercel.app/',
+    site_name: 'とみーの個人ブログ',
+    images: [
       {
         'url': 'https://nextjs-personal-blog-five.vercel.app/MyProfileImage.jpg',
         'width': 800,
