0f7a514449e8412e7e04271d26a14c2523dc4fc2
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-09-13 23:41:28 +0900

    やりたいことや指向性を追加

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -5,6 +5,7 @@ import {
   readFileFromFileName,
   parseMdFile,
 } from '@lib/MdFileOperation'
+import { Text } from '@components/ui'
 
 export async function getStaticProps() {
   const mdFileNames = getMdFileFromDir('resume')
@@ -34,6 +35,36 @@ const Resume = ({ parseMarkdownContent }: InferGetStaticPropsType<typeof getStat
   return (
     <>
       <div className="w-6/12 m-auto mt-10">
+        <Text variant="title" className="mb-6">
+          やりたいこと
+        </Text>
+
+        <p className="mb-6 text-xs">
+          仕事としてやりたいことは、フロントエンドを仕事としてやってきているのでフロントエンドの開発をやっていきたいと思っています。中でもReactを実際に実務で使用してみたいと考えています。
+          <br />
+          理由としては、Reactの方がよりJavaScriptらしさが強い点や、わかりやすい規約にVueよりもベストプラクティスを追いやすい点が魅力で仕事でも使ってみたいと思いました。
+          <br />
+          <br />
+          技術の移り変わりやライブラリの発展のスピードが早くキャッチアップをしていて楽しいのでフロントエンドの勉強してますが、強いこだわりはなく、求められればバックエンドやインフラといった別のことにもチャレンジしてみたいと考えており、現職でもバックエンドに関しては一部担当しております。
+        </p>
+
+        <Text variant="title" className="mb-6">
+          志向性
+        </Text>
+
+        <p className="mb-6 text-xs">
+          変化を恐れず、常に新しい挑戦をして成長したいと思っています。
+          <br />
+          自分のスキルが他の会社で通用するのかという漠然とした不安があり、実際に副業を始めてみて知らなかった会社のフロントエンドの開発に従事したり、オフラインの勉強会やLT会に参加してみて他のエンジニアと交流してみるといったことをしてきました。
+          <br />
+          挑戦してみると遅れていた割には意外となんとかなることを知ったり、自分のスキルが意外と役に立っているという実感を副業先で知ることができました。
+          <br />
+          このように自身の成長を促してくれそうな挑戦を今後も続けていきたいと考えています。
+        </p>
+
+        <Text variant="title" className="mb-6">
+          職務経歴
+        </Text>
         {
           parseMarkdownContent.map((val, index) => {
             return (
